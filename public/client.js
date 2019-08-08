a5758bf36ed538e73069d960ee09ceaf118ad738
noodleWrecker7 <hodgkinson.adam@gmail.com>
2019-08-08 15:22:52 +0100

    set content type to gae

--- public/client.js
+++ public/client.js
@@ -1,7 +1,7 @@
 /*******************************************************************************
  * Copyright (c) 2019.
  * Developed by Adam Hodgkinson
- * Last modified 08/08/19 15:16
+ * Last modified 08/08/19 15:22
  ******************************************************************************/
 const LOCAL_AUTH_CODE_KEY = "controlify-sotify-auth-code";
 const LOCAL_ACCESS_TOKEN_KEY = "controlify-spotify-access-token";
@@ -26,6 +26,7 @@ document.addEventListener('DOMContentLoaded', function () {
         let req = new XMLHttpRequest(); // this goes to an app engine server which then goes to spotify
         req.open("GET", "https://controlify-backend.appspot.com");
         //req.setRequestHeader("auth_code", localStorage.getItem(LOCAL_AUTH_CODE_KEY));
+        req.setRequestHeader("Content-Type", "text/plain");
         req.onload = function () {
             let data = this.response;
             console.log(data);
