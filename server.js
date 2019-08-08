992e5416b02b1665ddacc093af9ada8616f03509
noodleWrecker7 <hodgkinson.adam@gmail.com>
2019-08-08 20:22:11 +0100

    no idea anymore

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 /*******************************************************************************
  * Copyright (c) 2019.
  * Developed by Adam Hodgkinson
- * Last modified 08/08/19 20:00
+ * Last modified 08/08/19 20:19
  ******************************************************************************/
 const http = require('http');
 const requester = require('request');
@@ -10,6 +10,15 @@ var app = express();
 const fs = require('fs');
 
 let authtext = null;
+
+function routes(router)  {
+    /** save a Black Panther character */
+    router
+        .route('/request-token')
+        .post(getAccessToken)
+}
+
+routes(express.Router())
 
 /*requester("/appauth.txt", function (err, resp, body) {
     console.error('error:', err); // Print the error if one occurred
@@ -24,7 +33,60 @@ fs.readFile("appauth.txt", "utf8", (err, data) => {
     console.log("Authtext = " + authtext);
 });
 
+app.listen(process.env.PORT || 8080, () => {
+    console.log("Listening on ${port}")
+})
 
+function getAccessToken(req, res, next) {
+    console.log("Received body: " + req.body);
+
+    let data = null;
+    console.log("Incoming request, type:" + req.method)
+    console.log("headers: " + req.headers)
+
+    let body = "grant_type=authorization_code&" +
+        "redirect_uri=https%3A%2F%2Fcontrolify.noodlewrecker.xyz%2F&" +
+        "code=" + req.body + "&" +
+        authtext;
+    console.log("Body to spotify: " + body)
+    let clientID = authtext.split("&")[0].split("=")[1]
+    let clientSecret = authtext.split("&")[1].split("=")[1]
+    requester({
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        uri: 'https://accounts.spotify.com/api/token',
+        //body: body,
+        data: {
+            grant_type: 'authorization_code',
+            redirect_uri: 'https%3A%2F%2Fcontrolify.noodlewrecker.xyz%2F&',
+            code: incomingBody,
+            client_id: clientID,
+            client_secret: clientSecret
+        },
+        method: 'POST'
+    }, function (err, resp, body) {
+        console.log("Received access_token")
+        //it works!
+        //data = this.response;
+        //response.statusCode = 200;
+        //response.setHeader('Content-Type', 'application/json');
+        //response.setHeader("Access-Control-Allow-Origin", request.headers.origin);
+        res.writeHead(200, {
+            'Content-Type': 'application/json',
+            "Access-Control-Allow-Origin" : "*"
+        });
+        res.write(body);
+        res.end();
+        console.log("complete");
+    });
+
+
+    next();
+}
+
+
+/*
 http.createServer((request, response) => {
 //app.get("/request-token", function (request, response) {
     console.log("Request Received")
@@ -98,5 +160,5 @@ http.createServer((request, response) => {
     };
     req.send(outBody).then();*/
 
-}).listen(process.env.PORT || 8080);
+//}).listen(process.env.PORT || 8080);
 console.log("Server started")
