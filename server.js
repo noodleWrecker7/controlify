e6de0d2ef429c440c42edab9be2ee956e63490b5
noodleWrecker7 <hodgkinson.adam@gmail.com>
2019-08-08 14:09:48 +0100

    added node shit

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
 /*******************************************************************************
  * Copyright (c) 2019.
  * Developed by Adam Hodgkinson
- * Last modified 07/08/19 23:16
+ * Last modified 08/08/19 14:09
  ******************************************************************************/
 
+const http = require('http');
+
+http.createServer((request, response) => {
+    console.log("Incoming request, type:" + request.method)
+    console.log("headers: " + request.headers)
+
+
+
+    response.statusCode = 200;
+    response.setHeader('Content-Type', 'application/json');
+    response.write("this is the json bit")
+    response.end();
+}).listen(8080);
